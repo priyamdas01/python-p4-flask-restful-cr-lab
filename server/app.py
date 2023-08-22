3d9f224741a0aa8ee824af53b12a2267f82d4ebb
Priyam Das <domidas01@gmail.com>
2023-08-22 02:36:02 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -16,12 +16,35 @@ db.init_app(app)
 
 api = Api(app)
 
+
 class Plants(Resource):
-    pass
+    def get(self):
+        response_body = [plant.to_dict() for plant in Plant.query.all()]
+        response = make_response(response_body, 200)
+        return response
+
+    def post(self):
+        new_plant = Plant(
+            name=request.form.get('name'),
+            image=request.form['image'],
+            price=request.form['price']
+        )
+        response_body = new_plant.to_dict()
+        response = make_response(response_body, 201)
+        return response
+
+
+api.add_resource(Plants, '/plants')
+
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        response_body = Plant.query.filter(Plant.id == id).first().to_dict()
+        response = make_response(response_body, 200)
+        return response
+
+
+api.add_resource(PlantByID, '/plants/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
